9658904c1da53db5348354b6ebe21b728869a2f1
sircharlie <esculin.hao@gmail.com>
2020-04-16 15:27:09 -0700

    interactive

--- src/app/piechart/piechart.component.ts
+++ src/app/piechart/piechart.component.ts
@@ -27,5 +27,40 @@ export class PiechartComponent implements OnInit {
       this.sectors[i].offset =
         this.sectors[i - 1].offset - this.sectors[i - 1].p;
     }
+
+    this.change(20, 50, 30);
+  }
+
+  spin(): void {
+    this.change(30, 40, 30);
+  }
+
+  spin2(): void {
+    this.change(1, 4, 95);
+  }
+
+  change(n1, n2, n3): void {
+    var val1D = document.getElementById('donut-segment1');
+    var val2D = document.getElementById('donut-segment2');
+    var val3D = document.getElementById('donut-segment3');
+    var per1 = n1;
+    var per2 = n2;
+    var per3 = n3;
+    var offset = 25;
+
+    val1D.style.transition =
+      'stroke-dasharray 0.5s ease-in-out, stroke-dashoffset 0.5s ease-in-out';
+    val1D.style.strokeDasharray = per1 + ' ' + (100 - per1);
+    val1D.style.strokeDashoffset = offset + '';
+
+    val2D.style.transition =
+      'stroke-dasharray 0.5s ease-in-out, stroke-dashoffset 0.5s ease-in-out';
+    val2D.style.strokeDasharray = per2 + ' ' + (100 - per2);
+    val2D.style.strokeDashoffset = 100 - per1 + offset + '';
+
+    val3D.style.transition =
+      'stroke-dasharray 0.5s ease-in-out, stroke-dashoffset 0.5s ease-in-out';
+    val3D.style.strokeDasharray = per3 + ' ' + (100 - per3);
+    val3D.style.strokeDashoffset = 100 - (per1 + per2) + offset + '';
   }
 }
